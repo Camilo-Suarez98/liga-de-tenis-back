441f88e234c3dab25d3fd002df5093b4ebedef8b
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-11 10:01:51 -0500

    include routes to make crud operatons

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 const express = require('express')
 const configExpress = require('./config/express')
 const connect = require('./config/database')
+const routes = require('./routes')
 
 const app = express()
 connect()
 
 configExpress(app)
+routes(app)
 
 module.exports = app
