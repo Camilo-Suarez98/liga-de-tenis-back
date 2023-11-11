29059745fde658d950a3279b426c2923138f1091
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-11 08:56:24 -0500

    connect mongodb to app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const configExpress = require('./config/express')
+const connect = require('./config/database')
 
 const app = express()
+connect()
 
 configExpress(app)
 
