51103c4fa2df8dab852d3e29c8c562149541a699
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-13 10:53:17 -0500

    include sign in verification to create, update and delete tournaments

--- src/api/tournament/index.js
+++ src/api/tournament/index.js
@@ -6,12 +6,13 @@ const {
   updateTournamentController,
   deleteTournamentController
 } = require('./tournament.controller')
+const { isAuthenticated } = require('../../auth/auth.controller')
 
 router.route('/').get(getTournamentsController)
-router.route('/').post(createTournamentController)
+router.route('/').post(isAuthenticated, createTournamentController)
 
 router.route('/:id').get(getTournamentByIdController)
-router.route('/:id').put(updateTournamentController)
-router.route('/:id').delete(deleteTournamentController)
+router.route('/:id').put(isAuthenticated, updateTournamentController)
+router.route('/:id').delete(isAuthenticated, deleteTournamentController)
 
 module.exports = router
