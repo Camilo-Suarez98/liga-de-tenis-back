4a1e02b88e785b8365d26c0bab519634ca62e4b2
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2023-11-13 21:47:44 -0500

    change verification in tournaments

--- src/api/tournament/index.js
+++ src/api/tournament/index.js
@@ -6,13 +6,12 @@ const {
   updateTournamentController,
   deleteTournamentController
 } = require('./tournament.controller')
-const { isAuthenticated } = require('../../auth/auth.controller')
 
 router.route('/').get(getTournamentsController)
-router.route('/').post(isAuthenticated, createTournamentController)
+router.route('/').post(createTournamentController)
 
 router.route('/:id').get(getTournamentByIdController)
-router.route('/:id').put(isAuthenticated, updateTournamentController)
-router.route('/:id').delete(isAuthenticated, deleteTournamentController)
+router.route('/:id').put(updateTournamentController)
+router.route('/:id').delete(deleteTournamentController)
 
 module.exports = router
